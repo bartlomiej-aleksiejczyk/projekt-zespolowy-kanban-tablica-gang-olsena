238114e2319445137736ab0e7e26ee75bbe11ffb
Patryk Adamowicz <patrykadam.dev@gmail.com>
2023-02-27 21:55:32 +0100

    Adding move, delete, get API url

--- backend/kanban/urls.py
+++ backend/kanban/urls.py
@@ -1,22 +1,52 @@
 from django.urls import path
 from kanban.views.board_view import BoardViewSet
+from kanban.views.card_view import CardViewSet
+
+single_board_viewset = BoardViewSet.as_view(
+    dict(
+        get='get_board',
+        delete='delete_board'
+    )
+)
 
 board_viewset = BoardViewSet.as_view(
-	dict(
-		post='create',
-		get='get_all'
-	)
+    dict(
+        post='create_board',
+        get='get_boards'
+    )
 )
 
-single_board_card_viewset = BoardViewSet.as_view(
-	dict(
-		post='create_card',
-		get='get_cards',
-	)
+board_move_viewset = BoardViewSet.as_view(
+    dict(
+        post='move_board'
+    )
 )
 
+card_move_viewset = CardViewSet.as_view(
+    dict(
+        post='move_card'
+    )
+)
+
+single_card_viewset = CardViewSet.as_view(
+    dict(
+        get='get_card',
+        delete='delete_card'
+    )
+)
+
+board_card_viewset = BoardViewSet.as_view(
+    dict(
+        post='create_board',
+        get='get_board_cards'
+    )
+)
 
 urlpatterns = [
     path('board/', board_viewset),
-	path('board/<int:pk>/cards/', single_board_card_viewset)
+    path('board/<int:pk>/', single_board_viewset),
+    path('board/<int:pk>/move/', board_move_viewset),
+    path('card/<int:pk>/', single_card_viewset),
+    path('card/<int:pk>/move/', card_move_viewset),
+    path('board/<int:pk>/card/', board_card_viewset)
 ]
